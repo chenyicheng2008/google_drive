e95a61bee653e2818c672710f5860ca12225621d
陳一誠 <68698038+chenyicheng2008@users.noreply.github.com>
2025-12-16 01:02:19 +0800

    Add script to download files from Google Drive

---
+++ your_download_script.py
@@ -0,0 +1,18 @@
+from pydrive2.auth import GoogleAuth
+from pydrive2.drive import GoogleDrive
+
+# 1. 服務帳號認證
+gauth = GoogleAuth()
+# 使用 JSON 檔案進行非互動式認證
+gauth.settings['client_config_file'] = 'client_secrets.json' 
+gauth.settings['oauth_scope'] = ['https://www.googleapis.com/auth/drive']
+gauth.settings['save_credentials'] = False # 不儲存憑證
+gauth.ServiceAuth()
+
+drive = GoogleDrive(gauth)
+
+# 2. 下載檔案 (使用檔案ID)
+file_id = 'YOUR_FILE_ID_ON_DRIVE' 
+file = drive.CreateFile({'id': file_id})
+file.GetContentFile('downloaded_file.txt')
+print(f"File {file['title']} downloaded successfully.")
